f5d74ba32114d82d6576c31df8581211019e800e
rastislavkopal <rasto.kopal@gmail.com>
2021-02-28 16:31:44 +0100

    refactored api and form handling

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,9 +1,9 @@
-$(document).ready( function () {
+function updateDataTable()
+{
     let url = "http://147.175.98.78/cv2/handlers/GetTableDataHandler.php";
-    let searchParams = new URLSearchParams(window.location.search)
+    let searchParams = new URLSearchParams(window.location.search);
     if (searchParams.has('subdir'))
-        url += "?subdir=" +searchParams.get('subdir')
-
+        url += "?subdir=" +searchParams.get('subdir');
     $.get(url,
         function (data) {
             let json = JSON.parse(data);
@@ -14,9 +14,13 @@ $(document).ready( function () {
                 "bInfo": false,
                 "scrollY":"80%",
                 "scrollCollapse": true,
+                "destroy": true,
             });
+        });
+}
 
-    });
+$(document).ready( function () {
+    updateDataTable()
 } );
 
 function showUploadForm()
@@ -32,18 +36,34 @@ $("#file-upload").change(function(){
     $("#file-name").text(this.files[0].name);
 });
 
-$("#addFileButton").on('click', function(){
-    window.location.replace("http://147.175.98.78/cv2");
-});
-
 function displayMessage(response)
 {
+    updateDataTable();
     $('#uploader_div').hide('slow');
     var message = $('<div class="alert alert-error error-message" style="display: none;">');
     var close = $('<button type="button" class="close" data-dismiss="alert">&times</button>');
     message.append(close); // adding the close button to the message
     message.append(response);
     message.appendTo($('body')).fadeIn(300).delay(3000).fadeOut(1500);
+}
+
+function clearFormInputs() // clear forms after upload
+{
+    $("#uploadedFileName").val('');
+    $("#file-upload").val('');
+    $("#file-name").text('');
+}
+
+function apiSaveUploadedFile(dataObject)
+{
+    $.ajax({
+        url: 'http://147.175.98.78/cv2/handlers/fileHandler.php',
+        type: 'post',
+        data: dataObject,
+        contentType: false,
+        processData: false,
+        success: displayMessage,
+    });
 }
 
 $("#post-btn").click(function(){
@@ -55,22 +75,17 @@ $("#post-btn").click(function(){
     var fd = new FormData(form);
 
     // Check file selected or not
-    if(form.length > 0 ){
+    if($("#file-upload").val()){
         fd.append('file-upload',form[0]);
         fd.append('uploadedFileName', $('input[type=file]')[0].files[0]);
         let searchParams = new URLSearchParams(window.location.search)
         if (searchParams.has('subdir'))
             fd.append('uploadedFilePath', searchParams.get('subdir'));
 
-        $.ajax({
-            url: 'http://147.175.98.78/cv2/handlers/fileHandler.php',
-            type: 'post',
-            data: fd,
-            contentType: false,
-            processData: false,
-            success: displayMessage,
-        });
+        clearFormInputs();
+        apiSaveUploadedFile(fd);
     }else{
-        alert("Please select a file.");
+        clearFormInputs();
+        displayMessage("Prosím, vyberte súbor.");
     }
 });
--- handlers/fileHandler.php
+++ handlers/fileHandler.php
@@ -12,32 +12,12 @@ if (isset($_POST['uploadedFilePath']) && $_POST['uploadedFilePath'] != '.'){
 if (isset($_POST['uploadedFileName']))
     $target_file = $target_dir . $_POST['uploadedFileName'];
 
-$uploadOk = 1;
 
 $fileType = strtolower(pathinfo($_FILES["file-upload"]["name"],PATHINFO_EXTENSION));
 $target_file .= '.' . $fileType . generateRandomString(10);
 
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-
-// Check file size
-if ($_FILES["file-upload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
+if (move_uploaded_file($_FILES["file-upload"]["tmp_name"], $target_file)) {
+    echo "Súbor '". $_POST['uploadedFileName']. '.' . $fileType . "' bol úspešne pridaný.";
 } else {
-    if (move_uploaded_file($_FILES["file-upload"]["tmp_name"], $target_file)) {
-        echo "Súbor '". htmlspecialchars( $_POST['uploadedFileName'] ). '.' . $fileType . "' bol úspešne pridaný.";
-    } else {
-        echo "Sorry, there was an error uploading your file with path:" . substr($target_file, 0, -10);
-    }
+    echo "Nastala chyba pri uploade súboru: " . substr($target_file, 0, -10);
 }
-?>
